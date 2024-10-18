5e4f435377526d27f45a82e74ce8c788b7c82dfb
Jacob-Lundstrom <Jacob.Lundstrom.EE@gmail.com>
2024-10-17 22:37:55 -0500

    Simplified Interfacing

--- AD9833.py
+++ AD9833.py
@@ -9,14 +9,14 @@ import RPi.GPIO as GPIO
 import time
 
 
-freq = 20_000_000 #Max SPI Frequency for the AD9833 is 40 MHz
+AD9833_SPI_freq = 20_000_000 #Max SPI Frequency for the AD9833 is 40 MHz
 
 #FROM EXTERNAL SOURCE
 master_clk = 1_000_000 # Has a direct relation to the output waveform frequency
 
 
 
-def AD9833_write(spi, data_high, data_low):
+def AD9833_write(data_high, data_low):
     """
     Write 2 bytes to the specified register.
     """
@@ -26,7 +26,7 @@ def AD9833_write(spi, data_high, data_low):
 
     spi = spidev.SpiDev()
     spi.open(bus,device)
-    spi.max_speed_hz = freq 
+    spi.max_speed_hz = AD9833_SPI_freq 
     spi.mode = 2
 
     data_out = bytearray([data_high, data_low])
@@ -34,7 +34,7 @@ def AD9833_write(spi, data_high, data_low):
 
     spi.close()
 
-def AD9833_write4(spi, data_l_high, data_l_low, data_h_high, data_h_low):
+def AD9833_write4(data_l_high, data_l_low, data_h_high, data_h_low):
     """
     Write 4 bytes to the specified register.
     """
@@ -44,7 +44,7 @@ def AD9833_write4(spi, data_l_high, data_l_low, data_h_high, data_h_low):
 
     spi = spidev.SpiDev()
     spi.open(bus,device)
-    spi.max_speed_hz = freq 
+    spi.max_speed_hz = AD9833_SPI_freq 
     spi.mode = 2
 
     data_out = bytearray([data_l_high, data_l_low, data_h_high, data_h_low])
@@ -56,18 +56,10 @@ def AD9833_set_freq(f):
     # should be used with the Master input clock frequency of 10 MHz
     global master_clk
 
-    spi = spidev.SpiDev()
-    spi.open(0, 1)  # Bus 0, Device 1
-    spi.max_speed_hz = freq
-
-    #AD9833_write(spi, 0b0010_0000, 0b0000_0000)
     
-    AD9833_write( spi, 0b0010_0000, 0b1000_0000)  # Control reg, Start reset
+    AD9833_write(0b0010_0000, 0b1000_0000)  # Control reg, Start reset
 
-    FRQREG = int((f) * (2**28) / (master_clk))  # Page 15
-    #53687
-
-    
+    FRQREG = int((f) * (2**28) / (master_clk)) 
 
     # Starting with the lower bytes
     FL = FRQREG & 0x3F_FF
@@ -79,20 +71,14 @@ def AD9833_set_freq(f):
     Hlb = FH & 0xFF
     Hhb = ((FH & 0x3F_00) >> 8) + 0b0100_0000
 
-    AD9833_write4(spi,Lhb, Llb, Hhb, Hlb)
+    AD9833_write4(Lhb, Llb, Hhb, Hlb)
 
-    # All the bytes being transmitted are correct
-
-    AD9833_write( spi, 0b0010_0000, 0b0000_0000) # Control reg, finish reset and configure ic
+    AD9833_write(0b0010_0000, 0b0000_0000) # Control reg, finish reset and configure ic
 
 def AD9833_Init():
     # Initialize AD9833
-    spi = spidev.SpiDev()
-    spi.open(0, 1)  # Bus 0, Device 1
-    spi.max_speed_hz = freq
-
-    AD9833_write(spi, 0b0001_0000, 0b0000_0000)
-    AD9833_write(spi, 0b0100_0000, 0b1111_1111)
+    AD9833_write(0b0001_0000, 0b0000_0000)
+    AD9833_write(0b0100_0000, 0b1111_1111)
 
 def main():
     AD9833_Init()
